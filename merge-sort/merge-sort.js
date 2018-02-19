5c37eca0f9ef9fbea55d2232167fa2c60cac9a26
Hitesh Kumar <hk.skit@gmail.com>
2018-02-19 23:18:05 +0530

    Merge Sort:
         * Implemented merge sort algorithm.
         * Wrote unit test cases.

---
+++ merge-sort/merge-sort.js
@@ -0,0 +1,50 @@
+const { signum } = require("../utils");
+const merge = (array, cb = signum, l, m, r) => {
+  // Populate subarray to array to temp arrays.
+  const left = [];
+  while (left.length < m - l + 1) {
+    left.push(array[left.length + l]);
+  }
+  const right = [];
+  while (right.length < r - m) {
+    right.push(array[right.length + m + 1]);
+  }
+
+  // Merging left and right arrays to main array.
+  let i = 0;
+  let j = 0;
+  let k = l; // start index for merge to start.
+  while (i < left.length && j < right.length) {
+    if (cb.call(array, left[i], right[j]) < 0) {
+      array[k] = left[i];
+      i += 1;
+    } else {
+      array[k] = right[j];
+      j += 1;
+    }
+    k += 1;
+  }
+
+  // Populating remaining items.
+  while (i < left.length) {
+    array[k] = left[i];
+    i += 1;
+    k += 1;
+  }
+
+  while (j < right.length) {
+    array[k] = right[j];
+    j += 1;
+    k += 1;
+  }
+};
+const mergeSort = (array, cb = signum, l = 0, r = array.length - 1) => {
+  if (l >= r) {
+    return;
+  }
+  const m = Math.floor(l + (r - l) / 2);
+  mergeSort(array, cb, l, m);
+  mergeSort(array, cb, m + 1, r);
+  merge(array, cb, l, m, r);
+};
+module.exports = mergeSort;
---
+++ merge-sort/merge-sort.spec.js
@@ -0,0 +1,39 @@
+const should = require("should");
+const { expect } = require("chai");
+const mergeSort = require("./merge-sort");
+
+describe("mergeSort:", function() {
+  it("should sort an empty array", function() {
+    const array = [];
+    mergeSort(array);
+    expect(array.length).equal(0);
+  });
+
+  it("should sort an array", function() {
+    const array = [2, 1, 3, 5, 4];
+    const result = [1, 2, 3, 4, 5];
+    mergeSort(array);
+    result.forEach((item, index) => {
+      expect(item).equal(array[index]);
+    });
+  });
+
+  it("should sort an array in ascending order", function() {
+    const array = [5, 4, 3, 2, 1];
+    const result = [1, 2, 3, 4, 5];
+    mergeSort(array);
+    result.forEach((item, index) => {
+      expect(item).equal(array[index]);
+    });
+  });
+
+  it("should sort array in descending order", function() {
+    const array = [1, 2, 3, 4, 5];
+    const result = [5, 4, 3, 2, 1];
+    mergeSort(array, (x, y) => y - x);
+    console.log(array);
+    result.forEach((item, index) => {
+      expect(item).equal(array[index]);
+    });
+  });
+});
