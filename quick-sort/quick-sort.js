baa5ce56781887762166fbe8fe685c970fd67a6a
Hitesh Kumar <hk.skit@gmail.com>
2018-02-28 23:14:52 +0530

    Quick Sort:
    	* Implemented merge sort algorithm.
        * Wrote unit test cases.

---
+++ quick-sort/quick-sort.js
@@ -0,0 +1,27 @@
+const { signum } = require("../utils");
+const partition = (array, cb, low, high) => {
+  const pivot = array[high];
+  let i = low - 1; // Smaller element index.
+  for (let j = low; j < high; j += 1) {
+    if (cb.call(array, array[j], pivot) <= 0) {
+      i += 1;
+      // swapping using es6
+      [array[j], array[i]] = [array[i], array[j]];
+    }
+  }
+  i += 1;
+  [array[i], array[high]] = [array[high], array[i]];
+  return i;
+};
+const quickSort = (array, cb = signum, low = 0, high = array.length - 1) => {
+  if (low >= high) {
+    return;
+  }
+  const pi = partition(array, cb, low, high);
+  // sort left part.
+  quickSort(array, cb, low, pi - 1);
+  // sort right part.
+  quickSort(array, cb, pi + 1, high);
+};
+
+module.exports = quickSort;
